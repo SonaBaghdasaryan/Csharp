e803c9295bcd62be291152eec7312c51b013a892
Sona <sona.bagda91@gmail.com>
2022-06-09 20:39:58 +0300

    The first task completed

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+
+int a, b, max;
+
+Console.WriteLine("Input the first number");
+a=Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input the second number");
+b=Convert.ToInt32(Console.ReadLine());
+
+
+if (a > b)
+{
+    Console.WriteLine($"Max number is {a} ");
+}
+
+if (b > a)
+
+{
+    Console.WriteLine($"Max number {b} ");
+}
+
+*/
+
+
