b9de2152d97c12b49e0991bd08b89352bf56ae72
Sona <sona.bagda91@gmail.com>
2022-06-09 21:08:44 +0300

    The second task completed

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -22,4 +22,42 @@ if (b > a)
 
 */
 
+int a, b, c;
 
+Console.WriteLine("Input the first number");
+a=Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input the second number");
+b=Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input the third number");
+c=Convert.ToInt32(Console.ReadLine());
+
+if (a > b) 
+{
+  if (a > c)  
+    {
+        Console.WriteLine($"Max number is {a} ");
+    }
+
+}
+
+
+if (b > a) 
+{
+    if (b > c)
+    {
+     Console.WriteLine($"Max number {b} ");
+    }
+
+}
+    
+
+if (c > a) 
+{
+    if (c > b)
+    {
+     Console.WriteLine($"Max number {c} ");
+    }
+    
+}
